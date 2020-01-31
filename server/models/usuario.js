e7c0b2b40007d83672e9e2b602eff6f6a01d5ad0
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-01-31 13:39:06 -0600

    video 97  + Bcrypt - Encriptando la contraseña

--- server/models/usuario.js
+++ server/models/usuario.js
@@ -41,6 +41,15 @@ let usuarioSchema = new Schema({
     }
 });
 
+usuarioSchema.methods.toJSON = function() {
+
+    let user = this;
+    let userObject = user.toObject();
+    delete userObject.password;
+
+    return userObject;
+}
+
 usuarioSchema.plugin( uniqueValidator, {
     message: '{PATH} debe de ser único'
 });
