619f7306f5f63dabb2c9af278e8a841d5383f357
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-02-07 13:24:24 -0600

    video 112  + Variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -17,7 +17,7 @@ let urlDB;
 if ( process.env.NODE_ENV ==='dev' ) {
     urlDB =  'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://cor:xbUtPpuPmIseVUGT@cluster0-jols1.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 // ==================================================================
 
