40b2b5e82d7a0d2085afa8720f22b4902dbe43e9
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-02-12 13:16:10 -0600

    video 132  + Obtener Api key y api secret de Google  + Exponer carpeta publica  + Interfaz sencilla para login con Google

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ require('./config/config');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 const bodyParser = require('body-parser');
@@ -13,6 +14,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
+
+// Habilitar el public
+app.use(express.static(path.resolve(__dirname, '../public')));
 
 // Congiguración global de rutas
 app.use( require('./routes/index') );
