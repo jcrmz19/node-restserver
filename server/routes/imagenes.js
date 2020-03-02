69a35449910703e8d3738442fe64b13fd1553bb4
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-02 16:43:47 -0600

    Video 163  + Middleware: Verifica token por url

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -2,13 +2,11 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
-const { verificaToken } = require('../middlewares/autenticacion');
+const { verificaTokenImg } = require('../middlewares/autenticacion');
 
 const app = express();
 
-
-
-app.get('/imagen/:tipo/:img', verificaToken, (req, res) => {
+app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
 
     let tipo = req.params.tipo;
     let img = req.params.img;
