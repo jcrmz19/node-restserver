aea3ec15315a7e1e3c0c83fad00c7577993aee34
Jose Carlos Ramirez Moreno <jramirezm@aic.pgjguanajuato.gob.mx>
2020-03-02 15:52:23 -0600

    video 160  + Cargar imagenes de productos

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -3,6 +3,7 @@ const fileUpload = require('express-fileupload');
 const app = express();
 
 const Usuario = require('../models/usuario');
+const Producto = require('../models/producto');
 
 const fs = require('fs');
 const path = require('path');
@@ -17,26 +18,25 @@ app.put('/upload/:tipo/:id', function(req, res) {
 
     if (!req.files) {
         return res.status(400)
-            .json({
-                ok: false,
-                err: {
-                    message: 'No se ha seleccionado ningún archivo'
-                }
-            });
+        .json({
+            ok: false,
+            err: {
+                message: 'No se ha seleccionado ningún archivo'
+            }
+        });
     }
 
     // Validar tipo
     let tiposValidos = ['productos', 'usuarios'];
     if ( tiposValidos.indexOf( tipo ) < 0 ) {
         return res.status(400)
-            .json({
-                ok: false,
-                err: {
-                    message: 'Los tipos permitidos son ' + tiposValidos.join(', ')
-                }
-            });
+        .json({
+            ok: false,
+            err: {
+                message: 'Los tipos permitidos son ' + tiposValidos.join(', ')
+            }
+        });
     }
-
 
     let archivo = req.files.archivo;
 
@@ -47,18 +47,19 @@ app.put('/upload/:tipo/:id', function(req, res) {
 
     if ( extensionesValidas.indexOf( extension ) < 0 ) {
         return res.status(400)
-            .json({
-                ok: false,
-                err: {
-                    message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
-                    ext: extension
-                }
-            });
+        .json({
+            ok: false,
+            err: {
+                message: 'Las extensiones permitidas son ' + extensionesValidas.join(', '),
+                ext: extension
+            }
+        });
     }
 
-    // Cambiar nombre al archivo
+    // Calcular nombre para servidor
     let nombreArchivo = `${ id }-${ Date.now() }.${ extension }`;
 
+    // Copiar archivo al servidor
     archivo.mv(`uploads/${ tipo }/${ nombreArchivo }`, (err) => {
 
         if (err) {
@@ -68,8 +69,14 @@ app.put('/upload/:tipo/:id', function(req, res) {
             });
         }
 
-        // Aquí, imagen cargada
-        imagenUsuario(id, res, nombreArchivo);
+        // Si llega aquí, yas e cargo la imagen al servidor, ahora actualizar la base de datos
+
+        if ( tipo === 'usuarios') {
+            imagenUsuario(id, res, nombreArchivo);
+        } else if ( tipo === 'productos') {
+            imagenProducto(id, res, nombreArchivo);
+        }
+
 
     });
 
@@ -113,15 +120,52 @@ function imagenUsuario( id, res, nombreArchivo ) {
                 usuario: usuarioGuardado,
                 img: nombreArchivo
             })
-
         });
-
     });
-
 }
 
-function imagenProducto() {
+function imagenProducto( id, res, nombreArchivo ) {
 
+    Producto.findById( id, (err, productoDB) => {
+
+        if (err) {
+
+            borraArchivo( nombreArchivo, 'productos' );
+
+            return res.status(500).json({
+                ok: false,
+                err
+            });
+        }
+
+        if ( !productoDB ) {
+
+            borraArchivo( nombreArchivo, 'productos' );
+
+            return res.status(400)
+            .json({
+                ok: false,
+                err: {
+                    message: 'Producto no existe'
+                }
+            });
+        }
+
+        // Borrar la imagen anterior
+        borraArchivo( productoDB.img, 'productos' );
+
+        // Actualizar el nombre apuntando a la nueva imagen
+        productoDB.img = nombreArchivo;
+
+        productoDB.save( (err, productoGuardado) => {
+
+            res.json({
+                ok: true,
+                producto: productoGuardado,
+                img: nombreArchivo
+            })
+        });
+    });
 }
 
 function borraArchivo( nombreImagen, tipo ) {
